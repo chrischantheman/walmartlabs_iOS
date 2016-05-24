fb6f9fb3e1c77c83c4f51335983f371a907e7a49
chrischantheman <christopher.yh.chan@gmail.com>
2016-05-23 17:23:04 -0700

    Week 1 hw submission
    part 2

---
+++ fake_dropboxTests/fake_dropboxTests.swift
@@ -0,0 +1,36 @@
+//
+//  fake_dropboxTests.swift
+//  fake_dropboxTests
+//
+//  Created by Christopher Chan on 5/20/16.
+//  Copyright © 2016 Christopher Chan. All rights reserved.
+//
+
+import XCTest
+@testable import fake_dropbox
+
+class fake_dropboxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
